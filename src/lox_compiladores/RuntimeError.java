4f86b2629cb74ff3759a991b2c7fd9a2ce0e71c8
Gam13 <joaovictorgam13@gmail.com>
2025-07-13 15:57:09 -0300

    serial?

--- src/lox_compiladores/RuntimeError.java
+++ src/lox_compiladores/RuntimeError.java
@@ -1,6 +1,7 @@
 package lox_compiladores;
 
 class RuntimeError extends RuntimeException {
+	private static final long serialVersionUID = 3045665823372545478L;
   final Token token;
 
   RuntimeError(Token token, String message) {
