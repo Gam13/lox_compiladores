1a77700c7f553de1b9d1d26415a59174d1a96208
Gam13 <joaovictorgam13@gmail.com>
2025-07-13 16:53:10 -0300

    ongoing LoxFunc

--- src/lox_compiladores/Environment.java
+++ src/lox_compiladores/Environment.java
@@ -2,7 +2,11 @@ package lox_compiladores;
 
 public class Environment {
 
-    public Object getAt(Integer distance, Object lexeme) {
+    public Environment(Environment closure) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getAt(Integer distance, Object lexeme) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getAt'");
     }
@@ -11,5 +15,10 @@ public class Environment {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'get'");
     }
+
+	public void define(String string, LoxInstance instance) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/lox_compiladores/LoxFunc.java
+++ src/lox_compiladores/LoxFunc.java
@@ -3,11 +3,20 @@ package lox_compiladores;
 import java.util.List;
 
 public class LoxFunc implements LoxCallable {
+	
+	private final Stmt.Function declaration;
+	private final Environment closure;
+	private final boolean isInitializer;
+	
+	public LoxFunc(Stmt.Function declaration, Environment closure, boolean isinitializer) {
+		this.declaration = declaration;
+		this.closure = closure;
+		this.isInitializer = isinitializer;
+	}
 
 	@Override
 	public int ParamNumbs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return declaration.params.size();
 	}
 
 	@Override
@@ -16,9 +25,12 @@ public class LoxFunc implements LoxCallable {
 		return null;
 	}
 
-	public LoxClasses bind(LoxInstance instance) {
-		// TODO Auto-generated method stub
-		return null;
+	public LoxFunc bind(LoxInstance instance) {
+		Environment environment = new Environment(closure);
+		environment.define("this", instance);
+		return new LoxFunc(declaration,environment,isInitializer);
 	}
-
+	public String toString() {
+		return "<fn " + declaration.name.lexeme+ ">";
+	}
 }
