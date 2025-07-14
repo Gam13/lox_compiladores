ab83acf0db1d2926e8934a43ed183c7e99925920
Gam13 <joaovictorgam13@gmail.com>
2025-07-13 21:03:22 -0300

    loxInstance

--- src/lox_compiladores/LoxInstance.java
+++ src/lox_compiladores/LoxInstance.java
@@ -1,9 +1,33 @@
 package lox_compiladores;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoxInstance {
+    private LoxClasses klass;
+    private final Map<String, Object> fields = new HashMap<>();
 
-	public LoxInstance(LoxClasses loxClasses) {
-		// TODO Auto-generated constructor stub
-	}
+    public LoxInstance(LoxClasses klass) {
+        this.klass = klass;
+    }
 
+    public Object get(Token name) {
+        if (fields.containsKey(name.lexeme)) {
+            return fields.get(name.lexeme);
+        }
+
+        LoxFunc method = klass.findMethod(name.lexeme);
+        if (method != null) return method.bind(this);
+
+        throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
+    }
+
+    public void set(Token name, Object value) {
+        fields.put(name.lexeme, value);
+    }
+
+    @Override
+    public String toString() {
+        return klass.name + " instance";
+    }
 }
